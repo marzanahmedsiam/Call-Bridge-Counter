40d048e285438433b6e77954501bdfc06c1e056c
marzanahmedsiam <marzan40gen@gmail.com>
2024-05-30 14:30:15 +0600

    Change abar korsi dekh

--- src/main/java/org/example/callbridge/Main.java
+++ src/main/java/org/example/callbridge/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Ok");
         System.out.println("Siam is Juna's Daddy");
+        System.out.println("Lkhsi");
     }
 }
